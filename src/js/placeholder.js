f52a35f0e53fdf8e8f67f810b0d0616e52eae792
Greg Rickaby <gregrickaby@gmail.com>
2021-07-24 15:05:26 -0500

    add location to make it easy to find

--- src/js/placeholder.js
+++ src/js/placeholder.js
@@ -2,7 +2,9 @@
  * Placeholder. Safe to delete.
  */
 export default function placeholder() {
-  console.log("I'm a placeholder. Delete me!")
+  console.log(
+    "I'm just a placeholder. Navigate to /src/js/placeholder.js to delete me!"
+  )
 }
 
 placeholder()
